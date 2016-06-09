eddee0bd24a3e8d4f1053851cd5a8ff139a4592f
Joseph Tang <josephtang765@gmail.com>
2016-06-09 16:43:52 +0800

    test thing

--- index.js
+++ index.js
@@ -73,6 +73,7 @@ function sendTextMessage(sender, text) {
 }
 
 function sendGenericMessage(sender) {
+    
 	let messageData = {
 		"attachment": {
 			"type": "template",
@@ -104,13 +105,50 @@ function sendGenericMessage(sender) {
 			}
 		}
 	}
+    let messageData2 = {
+        attachment: {
+            type: "template",
+            payload: {
+                template_type: "generic",
+                elements: [{
+                    title: "rift",
+                    subtitle: "Next-generation virtual reality",
+                    item_url: "https://www.oculus.com/en-us/rift/",               
+                    image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+                    buttons: [{
+                        type: "web_url",
+                        url: "https://www.oculus.com/en-us/rift/",
+                        title: "Open Web URL"
+                    }, {
+                        type: "postback",
+                        title: "Call Postback",
+                        payload: "Payload for first bubble",
+                    }],
+                }, {
+                    title: "touch",
+                    subtitle: "Your Hands, Now in VR",
+                    item_url: "https://www.oculus.com/en-us/touch/",               
+                    image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+                    buttons: [{
+                        type: "web_url",
+                        url: "https://www.oculus.com/en-us/touch/",
+                        title: "Open Web URL"
+                    }, {
+                        type: "postback",
+                        title: "Call Postback",
+                        payload: "Payload for second bubble",
+                    }]
+                }]
+            }
+        }
+    }
 	request({
 		url: 'https://graph.facebook.com/v2.6/me/messages',
 		qs: {access_token:token},
 		method: 'POST',
 		json: {
 			recipient: {id:sender},
-			message: messageData,
+			message: messageData2,
 		}
 	}, function(error, response, body) {
 		if (error) {
