11065f4be187547217d93e8051d9754cd2da2aa4
Joseph Tang <josephtang765@gmail.com>
2016-06-09 16:18:24 +0800

    change toke

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ app.get('/', function (req, res) {
 
 // for facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+	if (req.query['hub.verify_token'] === 'giant_coffee_mug_explodes') {
 		res.send(req.query['hub.challenge'])
 	}
-	res.send('Error, wrong token')
+	res.send('Error, wrong token' + req.query['hub.verify_token'])
 })
 
 // to post data
